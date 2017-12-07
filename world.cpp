4c647fdbb4b649a3abe65b6a7ce920fa96da4d6a
rockerbacon <mp8andrade@hotmail.com>
2017-12-07 14:50:06 -0200

    implementou recuperacao de dados da malha, implementou leitura de mapa por arquivo

--- world.cpp
+++ world.cpp
@@ -1,4 +1,5 @@
 #include "world.h"
+#include <fstream>
 
 using namespace lab309;
 
@@ -12,7 +13,11 @@ World::World (const Window &window, size_t navmeshWidth, size_t navmeshHeight) {
 	}
 }
 
-Vector<int> World::mapToNavmesh (const Vector<float> &pos) {
+World::World (const Window &window) {
+	this->window = &window;
+}
+
+Vector<int> World::mapToNavmesh (const Vector<float> &pos) const {
 	if (pos[_X] < 0 || pos[_Y] < 0) {
 		return {-1, -1};
 	}
@@ -21,15 +26,87 @@ Vector<int> World::mapToNavmesh (const Vector<float> &pos) {
 	int cellHeight = this->window->getHeight()/this->navmesh.getLines();
 	Vector<int> v = (Vector<int>)pos;
 	
-	v[COORDINATE_X] = (v[_X] - v[_X] % cellWidth)/cellWidth;
-	v[COORDINATE_Y] = (this->window->getHeight() - (v[_Y] - v[_Y] % cellHeight) - cellHeight)/cellHeight;
+	v[_X] = (v[_X] - v[_X] % cellWidth)/cellWidth;
+	v[_Y] = (this->window->getHeight() - (v[_Y] - v[_Y] % cellHeight) - cellHeight)/cellHeight;
 	
 	return v;
 }
 
-void World::add (int id, const Vector<float> &pos) {
+Vector<float> World::mapToPixel (const Vector<int> &navPos) const {
+	Vector<float> pixel(2);
+	int gridWidth = this->window->getWidth()/this.navmesh.getColums();
+	int gridHeight = this->window->getHeight()/this.navmesh.getLines();
+	
+	pixel[_X] = pos[_X]*gridWidth;
+	pixel[_Y] = (this.navmesh.getLines() - pos[_Y] - 1) * gridHeight;
+	
+	return pixel;
+}
+
+Vector<int> World::add (int id, const Vector<float> &pos) {
 	Vector<int> navPos = this->mapToNavmesh(pos);
 	if (navPos[_X] != -1 && navPos[_X] < this->navmesh.getColums() && navPos[_Y] < this->navmesh.getLines()) {
 		this->navmesh[navPos] |= id;
 	}
+	return navPos;
+}
+
+std::list<Vector<float>> World::getFromMesh (int content) const {
+	Vector<int> pos(2);
+	std::list<Vector<float>> l;
+	for (pos[_X] = 0; pos[_X] < this.navmesh.getColums(); pos[_X]++) {
+		for (pos[_Y] = 0; pos[_Y] < this.navmesh.getLines(); pos[_Y]++) {
+			if ((this.navmesh[pos] & content) == content) {
+				l.push_front(mapToPixel(pos));
+			}
+		}	
+	}
+	return l;
+}
+
+bool World::readFromFile (const char *filePath) {
+	fsteam input;
+	size_t meshWidth, meshHeight;
+	char c;
+	
+	input.open(filePath, fstream::in);
+	if (input.fail()) {
+		return false;
+	}
+	
+	input >> meshWidth;
+	input >> meshHeight;
+	
+	this.navmesh = Matrix<Cell>(meshHeight, meshWidth);
+	for (size_t i = 0; i < this.navmesh.getLines(); i++) {
+		for (size_t j = 0; j < this.navmesh.getColums(); j++) {
+			//ignorar espacos, tabs e fins de linha
+			do {
+				input >> c;
+			} while (c==' '||c=='\n'||c=='\t');
+			//preencher celula da malha
+			switch(c) {
+				case WALL_CELL:
+					this.navmesh[i][j] = WALL_ID;
+				break;
+				case PACMAN_CELL:
+					this.navmesh[i][j] = PACMAN_ID|PATHWAY_ID;
+				break;
+				case GHOST_CELL:
+					this.navmesh[i][j] = GHOST_ID|PATHWAY_ID;
+				break;
+				case PILL_CELL:
+					this.navmesh[i][j] = PILL_ID|PATHWAY_ID;
+				break;
+				case SUPERPILL_CELL:
+					this.navmesh[i][j] = SUPERPILL_ID|PATHWAY_ID;
+				break;
+				default:
+					this.navmesh[i][j] = 0;
+				break;
+			} 
+		}
+	}	
+	
+	return true;
 }
--- world.h
+++ world.h
@@ -3,7 +3,7 @@
 #include "Lab309_ADT_Matrix.h"
 #include "window.h"
 #include "sprite.h"
-
+#include <list>
 
 //IDS DE OBJETOS
 #define PACMAN_ID		0x1
@@ -12,6 +12,14 @@
 #define PILL_ID			0x8
 #define SUPERPILL_ID	0x10
 #define WALL_ID			0x20
+
+//REPRESENTACAO DE MAPA EM ARQUIVO
+#define EMPTY_CELL		'0'
+#define WALL_CELL		'1'
+#define PACMAN_CELL		'k'
+#define GHOST_CELL		'y'
+#define PILL_CELL		'.'
+#define SUPERPILL_CELL	'*'
 
 namespace lab309 {
 	
@@ -22,12 +30,14 @@ namespace lab309 {
 				int contents;
 				
 			public:
-				
+				Cell (void) : contents(0) {}
+				Cell (int contents) : contents(contents) {}
 		};
 		
 		//INTERNAL METHODS
 		private:
-			Vector<int> mapToNavmesh (const Vector<float> &pos);
+			Vector<int> mapToNavmesh (const Vector<float> &pos) const;
+			Vector<float> mapToPixel (const Vector<int> &navPos) const;
 		
 		//ATRIBUTES
 		private:
@@ -37,8 +47,21 @@ namespace lab309 {
 		//METHODS
 		public:
 			World (const Window &window, size_t navmeshWidth, size_t navmeshHeight);
+			World (const Window &window);
 			
-			void add (int id, const Vector<float> &pos);	//adiciona objeto a malha de navegacao
+			Vector<int> add (int id, const Vector<float> &pos);	//adiciona objeto a malha de navegacao retornando a posicao na malha em que foi adicionado
+			
+			std::list<Vector<float>> getFromMesh (int content) const;	//retorna uma lista com a posicao (em pixels) de todos as celulas da malha que contenham as caracteristicas em content
+			
+			/*
+			 * Le uma malha de um arquivo
+			 * O arquivo deve conter:
+			 * 	-Dois inteiros iniciais indicando a largura e altura do mapa
+			 * 	-Caracteres representando seu conteudo de acordo com as macros no inicio desse arquivo
+			 * Espacos e tabs sao ignorados durante a leitura
+			 * Retorna true ao concluir ou false caso nao seja possivel ler o arquivo
+			 */
+			bool readFromFile (const char *filePath);
 	
 	};
 	
